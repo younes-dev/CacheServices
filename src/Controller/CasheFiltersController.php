4ad6562363f6a5c739f86be72ddc13f14e72ab65
younes-dev <younes.oulkaid@gmail.com>
2020-08-21 00:32:01 +0100

    Feat : Implement symfony AdapterInterface Cache

--- src/Controller/CasheFiltersController.php
+++ src/Controller/CasheFiltersController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
-use phpDocumentor\Reflection\Types\Object_;
+use DateInterval;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,11 +21,16 @@ class CasheFiltersController extends AbstractController
      * @var ArticleRepository
      */
     private $repository;
+    /**
+     * @var AdapterInterface
+     */
+    private $cache;
 
-    public function __construct(ArticleRepository  $repository)
+    public function __construct(ArticleRepository  $repository,AdapterInterface $cache)
     {
 
         $this->repository = $repository;
+        $this->cache = $cache;
     }
     //*************************************************************
     //************            Articles List             ***********
@@ -35,24 +39,45 @@ class CasheFiltersController extends AbstractController
      * @Route("/cashe/filters", name="cashe_filters")
      * @param Request $request
      * @return Response
+     * @throws InvalidArgumentException
      */
     public function articleList(Request $request):Response
     {
-        $articles=$this->repository->articlesPerPageNoFilter();
-        $enabledArticles=$this->repository->enabledArticles();
+        $cacheKey = 'thisIsACacheKey';
+        $item = $this->cache->getItem($cacheKey);
+        $cashItems=$item->get();
+        if ($item->isHit()) {
+            $articles = $cashItems['articles'] ? $cashItems['articles'] : null;
+            $enabledArticles = $cashItems['enabledArticles'] ? $cashItems['enabledArticles'] : null;
+            if ($request->isMethod('POST')) {
+                $start=$request->get("form")["start"]-1;
+                $end  =$request->get("form")["end"]+1   ;
+                $articleList = array_slice($articles, $start, $end);
+            }
+        }
+
         $formBuilder=self::FormMaker();
         $form = $formBuilder->getForm();
-        $articleList=null;
-        if ($request->isMethod('POST')) {
-            $start=$request->get("form")["start"]-1;
-            $end  =$request->get("form")["end"]+1;
-            $articleList=$this->repository->articlesPerPageNoFilter($end,$start);
+        if (!$item->isHit()) {
+            $articles=$this->repository->articlesPerPageNoFilter();
+            $enabledArticles=$this->repository->enabledArticles();
+            //********************************************************
+            $articleList=null;
+            if ($request->isMethod('POST')) {
+                $start=$request->get("form")["start"]-1;
+                $end  =$request->get("form")["end"]+1;
+                $articleList=$this->repository->articlesPerPageNoFilter($end,$start);
+            }
+            //********************************************************
+            $item->set(['articles' => $articles,'enabledArticles' => $enabledArticles,'articleList' => $articleList]);
+            $item->expiresAfter(new DateInterval('PT60S')); // the item will be cached for 10 seconds
+            $this->cache->save($item);
         }
+
         unset($formBuilder);
-        $list=$articleList;
         return $this->render('cashe_filters/index.html.twig', [
             'articles' => $articles,
-            'list' => $list,
+            'list' => $articleList,
             'enabledArticles' => $enabledArticles,
             'form' => $form->createView()
         ]);
@@ -76,16 +101,18 @@ class CasheFiltersController extends AbstractController
      */
     public function FormMaker(): FormBuilderInterface
     {
-        $attr=['style' => 'width: 160px', 'autofocus' => true, 'min' => 1, 'max' => 1000,];
+        $attr=['style' => 'width: 160px', 'autofocus' => true, 'min' => 1, 'max' => 1000,'placeholder' => 'Entrer un Id'];
         $formBuilder=$this->createFormBuilder();
         return $formBuilder
             ->add('start',IntegerType::class,[
+                'help' => 'The start code for your search filter.',
                 //'disabled' => true, 'data' => '2',
                 'label' =>'item Start',
                 'label_attr' => ['style' => "color:#467BFE;border:2px solid #467BFE;border-radius: 25% 10%;padding:1px;width:100px;text-align:center"],
                 'attr' => $attr
             ])
             ->add('end',IntegerType::class,[
+                'help' => 'The end code for your search filter.',
                 'label' =>'item End',
                 'label_attr' => ['style' => "color:#467BFE;border:2px solid #467BFE;border-radius: 25% 10%;padding:1px;width:100px;text-align:center"],
                 'attr' => $attr
